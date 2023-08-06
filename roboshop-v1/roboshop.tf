ed9375f9ba45e6cc4c4b4f4132713b268fc74585
Koheda Mahesh <maheshkoheda5@gmail.com>
2023-08-06 18:40:44 +0530

    terraform instances creation auto n route53

--- roboshop-v1/roboshop.tf
+++ roboshop-v1/roboshop.tf
@@ -24,6 +24,14 @@ resource "aws_instance" "mongodb" {
   }
 }
 
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z05775253JDMGSEH85KOY"
+  name    = "mongodb-dev.maheshkoheda.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.mongodb.private_ip]
+}
+
 resource "aws_instance" "catalogue" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
@@ -33,6 +41,15 @@ resource "aws_instance" "catalogue" {
   }
 }
 
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z05775253JDMGSEH85KOY"
+  name    = "catalogue-dev.maheshkoheda.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.catalogue.private_ip]
+}
+
+
 resource "aws_instance" "cart" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
@@ -40,6 +57,14 @@ resource "aws_instance" "cart" {
   tags = {
     Name = "cart"
   }
+}
+
+resource "aws_route53_record" "cart" {
+  zone_id = "Z05775253JDMGSEH85KOY"
+  name    = "cart-dev.maheshkoheda.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.cart.private_ip]
 }
 
 resource "aws_instance" "redis" {
@@ -51,6 +76,14 @@ resource "aws_instance" "redis" {
   }
 }
 
+resource "aws_route53_record" "redis" {
+  zone_id = "Z05775253JDMGSEH85KOY"
+  name    = "redis-dev.maheshkoheda.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.redis.private_ip]
+}
+
 resource "aws_instance" "user" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
@@ -58,6 +91,14 @@ resource "aws_instance" "user" {
   tags = {
     Name = "user"
   }
+}
+
+resource "aws_route53_record" "user" {
+  zone_id = "Z05775253JDMGSEH85KOY"
+  name    = "user-dev.maheshkoheda.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.user.private_ip]
 }
 
 resource "aws_instance" "mysql" {
@@ -69,6 +110,14 @@ resource "aws_instance" "mysql" {
   }
 }
 
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z05775253JDMGSEH85KOY"
+  name    = "mysql-dev.maheshkoheda.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.mysql.private_ip]
+}
+
 resource "aws_instance" "shipping" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
@@ -76,6 +125,14 @@ resource "aws_instance" "shipping" {
   tags = {
     Name = "shipping"
   }
+}
+
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z05775253JDMGSEH85KOY"
+  name    = "shipping-dev.maheshkoheda.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.shipping.private_ip]
 }
 
 resource "aws_instance" "rabbitmq" {
@@ -87,6 +144,14 @@ resource "aws_instance" "rabbitmq" {
   }
 }
 
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z05775253JDMGSEH85KOY"
+  name    = "rabbitmq-dev.maheshkoheda.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.rabbitmq.private_ip]
+}
+
 resource "aws_instance" "payment" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
@@ -94,4 +159,12 @@ resource "aws_instance" "payment" {
   tags = {
     Name = "payment"
   }
+}
+
+resource "aws_route53_record" "payment" {
+  zone_id = "Z05775253JDMGSEH85KOY"
+  name    = "payment-dev.maheshkoheda.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.payment.private_ip]
 }
