3e92a24a87a0358a7c7b6cc59889b167ba7d0f7d
Koheda Mahesh <maheshkoheda5@gmail.com>
2023-08-07 23:18:00 +0530

    terraform variables

--- 03-vars/sample.tf
+++ 03-vars/sample.tf
@@ -1,7 +1,37 @@
+#Plain variable
 variable "fruit_name" {
   default= "apple"
 }
 
 output "fruit_name" {
   value = var. fruit_name
+}
+#List variable
+variable "fruits" {
+  default = [
+  "apple",
+  "banana"
+  ]
+  // default = [ "apple", "banana" ]
+}
+
+#Map variable
+Variable "fruit_stock" {
+  default = {
+    apple = 100
+    banana = 200
+  }
+}
+#Map variable, Map of maps
+variable "fruit_stock_with_price" {
+  default = {
+    apple = {
+      stock = 100
+      price = 3
+    }
+    banana = {
+      stock = 400
+      price = 1
+    }
+  }
 }
---
+++ roboshop-v2/main.tf
@@ -0,0 +1,15 @@
+#variable "components" {
+#  default = [ "frontend", "mongodb", "catalogue", ]
+#
+#}
+
+#variable "components" {
+#  default = {
+#    frontend = {
+#      instance_type = "t3.micro"
+#    }
+#    mongodb = {
+#      intance_type = "t3.small"
+#    }
+#  }
+#}
