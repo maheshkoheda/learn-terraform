2cc80800de9a85154a68599b927517465de47456
Koheda Mahesh <maheshkoheda5@gmail.com>
2023-08-08 09:15:03 +0530

    terraform data sources

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -3,6 +3,23 @@ variable "fruits" {
 }
 
 output "fruit" {
-  //value = var.fruits[2]
+  //value = var.fruits[2] this will give error since 2nd fruit not available but element loops
   value = element(var.fruits, 2)
 }
+
+variable "fruit_with_stock" {
+  default = {
+    apple = {
+      stock = 100
+    }
+  }
+}
+
+output "fruit_stock" {
+  value = element(var.fruits, 2)
+}
+output "fruit_stock_price" {
+  value = lookup(var.fruit_wuth_stock["apple"], "price",2)
+  // try(var.fruit_with_stock["apple"].price, 2)
+}
+
