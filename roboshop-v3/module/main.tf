0dfdbb1be6f91433f2f4da7859fd7a9e21d48916
Koheda Mahesh <maheshkoheda5@gmail.com>
2023-08-11 13:21:10 +0530

    terraform modules and provisioner and depends on command

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -6,9 +6,7 @@ resource "aws_instance" "instance" {
   tags = {
     Name = var.name
   }
-  provisioner "local-exec" {
-    command = "sleep 60"
-  }
+
 }
 
 resource "aws_route53_record" "record" {
@@ -19,4 +17,20 @@ resource "aws_route53_record" "record" {
   records = [aws_instance.instance.private_ip ]
 }
 
+resource "null_reource" "ansible" {
+
+  depends_on = [
+    aws_route53_record.record
+  ]
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/roboshop-ansible
+git pull
+sleep 30
+ansible-playbook -i ${var.name}.maheshkoheda.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
+EOF
+
+  }
+}
 
